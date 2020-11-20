cec8572f43ed31137071c62e0ff25a936df9962f
Matheus M <matheus.martins.jeronimo@gmail.com>
2020-11-20 12:59:19 -0300

    Adiciona treino_perfil

---
+++ Treino_perfil/Perfil.cs
@@ -0,0 +1,18 @@
+﻿namespace Treino_Perfil
+{
+	class Pessoa
+	{
+		public string Nome { get; set; }
+		public string Endereco { get; set; }
+		public int Id { get; private set; }
+		public long Telefone { get; set; }
+
+		public Pessoa(string nome, string endereco, int id, long telefone)
+		{
+			Nome = nome;
+			Endereco = endereco;
+			Id = id;
+			Telefone = telefone;
+		}
+	}
+}
---
+++ Treino_perfil/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Treino_Perfil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Quem é voce?");
+
+            Console.Write("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Onde você mora? ");
+            string endereco = Console.ReadLine();
+
+            int id = 1000;
+
+            Console.Write("Qual é seu telefone de contato? ");
+            long telefone = long.Parse(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(nome, endereco, id, telefone);
+
+            Console.WriteLine("De acordo com meus cálculos você é...");
+            Console.WriteLine("Seu nome é {0}, você mora em {1} e seu telefone para contato é {2}. Na realização do seu cadastro associamos voce ao id {3}", pessoa.Nome, pessoa.Endereco, pessoa.Telefone, pessoa.Id);
+        }
+    }
+}
